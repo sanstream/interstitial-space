b3c0f4859601a277d75b20b9a00c2d9a35bccced
Sanne Peters <sanstream@pop-os.localdomain>
2021-10-22 16:33:52 +0200

    Drew a line on the grid in with an orthogonal perspective

--- interstitial-space.js
+++ interstitial-space.js
@@ -5,6 +5,7 @@ global.THREE = require("three");
 require("three/examples/js/controls/OrbitControls");
 
 const canvasSketch = require("canvas-sketch");
+const { GridHelper } = require("three")
 
 const settings = {
   // Make the loop animated
@@ -14,6 +15,11 @@ const settings = {
   context: "webgl"
 };
 
+const gridSize = {
+  width: 100,
+  height: 100,
+}
+
 const sketch = ({ context }) => {
   // Create a renderer
   const renderer = new THREE.WebGLRenderer({
@@ -21,20 +27,19 @@ const sketch = ({ context }) => {
   });
 
   // WebGL background color
-  renderer.setClearColor('black', 1);
+  renderer.setClearColor('#ffffff', 1);
 
   // Setup a camera
-  camera = new THREE.OrthographicCamera(
-    -10,
-    10,
-    10,
-    -10,
+  const camera = new THREE.OrthographicCamera(
+    -(gridSize.height / 2),
+    gridSize.height / 2,
+    gridSize.width / 2,
+    -(gridSize.width / 2),
     0.01,
-    100,
   );
   // const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
-  camera.position.set(9,9,9); // position camera
-  camera.lookAt(new THREE.Vector3());
+  camera.position.set(0,gridSize.height, 0); // position camera
+  camera.lookAt(0,0,0);
 
   // Setup camera controller
   const controls = new THREE.OrbitControls(camera, context.canvas);
@@ -46,22 +51,40 @@ const sketch = ({ context }) => {
   const geometry = new THREE.BoxGeometry(3,3,3);
 
   // Setup a material
-  const material = new THREE.MeshPhongMaterial({
+  const material = new THREE.MeshBasicMaterial({
     color: "red",
     wireframe: false
+  });
+
+  const lineMaterial = new THREE.LineBasicMaterial({
+    color: '#121212',
+    linewidth: 1,
   });
 
   // Setup a mesh with geometry + material
   const mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
+  mesh.position.set(0,0,0)
 
+  const points = [
+    new THREE.Vector3( 0, 0, 0 ),
+    new THREE.Vector3( 5, 0, 5 ),
+  ]
+  const lineGeometry = new THREE.BufferGeometry()
+    .setFromPoints(points)
+  const line = new THREE.Line(lineGeometry, lineMaterial)
+  scene.add(line)
   const light = new THREE.PointLight('white')
   scene.add(light)
-  light.position.set(7, 7, 0)
-  light.lookAt(0,0,0)
-  const lighthelper = new THREE.PointLightHelper(light)
-  scene.add(lighthelper)
-  // draw each frame
+  // light.position.set(7, 7, 0)
+  // light.lookAt(0,0,0)
+
+  // const lighthelper = new THREE.PointLightHelper(light)
+  // scene.add(lighthelper)
+
+  const gridHelper = new THREE.GridHelper(gridSize.height, 10, 'green')
+  scene.add(gridHelper)
+
   return {
     // Handle resize events here
     resize({ pixelRatio, viewportWidth, viewportHeight }) {
